49e89fc1fd4bff51d9a17832e7dc3627ec173e6e
Pramod Sankar L <pramodsankar.l@gmail.com>
2016-03-20 22:46:06 -0400

    Update dropdown.d.ts

--- dropdown.d.ts
+++ dropdown.d.ts
@@ -1,21 +1,21 @@
-  interface IDropDownViewModel extends ng.IScope{
-       vm: DropdownController;
-    }
+interface IDropDownViewModel extends ng.IScope{
+     vm: DropdownController;
+  }
 
-   interface IDropDownAttributes extends ng.IAttributes {
-        options: any;
-        settings: any
-   }
+ interface IDropDownAttributes extends ng.IAttributes {
+      options: any;
+      settings: any
+ }
 
-   interface IDropDownSettings {
-       performant: boolean;
-       sglItemLabel: boolean;
-   }
+ interface IDropDownSettings {
+     performant: boolean;
+     sglItemLabel: boolean;
+ }
 
-   interface ListItem {
-       text: string;
-       value: any;
-   }
+ interface ListItem {
+     text: string;
+     value: any;
+ }
 
 export {
   IDropDownViewModel,
